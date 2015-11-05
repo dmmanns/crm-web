61bc2817bc2a908432225400930089506b2f5699
dmmanns <dmmanns555@gmail.com>
2015-11-05 15:18:12 -0500

    performing delete contact working

--- contact.rb
+++ contact.rb
@@ -7,37 +7,41 @@ class Contact
   @@id = 1000
 
 #Used initialize method
-def initialize(first_name, last_name, email, notes)
-  @id = @@id
-  @first_name = first_name
-  @last_name = last_name
-  @email = email
-  @notes = notes
-  
-  @@id += 1
-end 
+  def initialize(first_name, last_name, email, notes)
+    @id = @@id
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @notes = notes
+    
+    @@id += 1
+  end 
 
-#create a new contact
-def self.create(first_name, last_name, email, notes)
-	@@contacts << Contact.new(first_name, last_name, email, notes)
-end
+  #create a new contact
+  def self.create(first_name, last_name, email, notes)
+  	@@contacts << Contact.new(first_name, last_name, email, notes)
+  end
 
-#find a contact for changing/modifcations  
-def self.find(contact_id)
-    @@contacts.find { |contact| contact.id == contact_id }
-end
+  #find a contact for changing/modifcations  
+  def self.find(contact_id)
+      @@contacts.find { |contact| contact.id == contact_id }
+  end
 
-# # This is a more advanced way of updating 
-# def update(field, new_value)
-# 	self.send("#{field}=", new_value)
-# end
+  # # This is a more advanced way of updating 
+  # def update(field, new_value)
+  # 	self.send("#{field}=", new_value)
+  # end
 
-def self.all
-    @@contacts
-end 
+  def remove
+    @@contacts.delete_if { |contact| contact.id == self.id }
+  end
 
-def full_name
-	"#{first_name} #{last_name}"
-end
+  def self.all
+      @@contacts
+  end 
+
+  def full_name
+  	"#{first_name} #{last_name}"
+  end
 
 end 
